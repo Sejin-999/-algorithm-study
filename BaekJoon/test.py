fce5a4481da4b792cf34689174e7361059c7b1e6
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-04-23 15:55:58 +0900

    4,23

--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -1,25 +1,14 @@
-text = input()
-resText = []
-tempText = []
-text_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-count = 0
+import sys
+n = int(sys.stdin.readline())
+# n = int(input())
+count = 0;
+sum = 0
 
-for i in text:
-    if i not in text_list or count + 1 == len(text):
-        if tempText != []:
-            resText.append("".join(tempText))
-            tempText = []
-    else:
-        tempText.append(i)
-
-    count += 1
-
-# 반복문 종료 후 tempText에 남아있는 단어들을 resText에 추가
-if tempText != []:
-    resText.append("".join(tempText))
-
-# 입력 문자열의 마지막 문자가 공백이 아니면 단어로 처리
-if text[-1] != ' ':
-    resText.append("".join(text[-1]))
-
-print(len(resText))
+for i in range(0,n) :
+    for j in range(0,n):
+        #sum += i*j
+        # print(f"{i} * {j} = {sum}")
+        count +=1
+        
+print(count)
+print(2) # i , j -> x x  -> 2
