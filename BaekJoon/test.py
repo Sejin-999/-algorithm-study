9845fcc467cf249bcadf3b2068e7e59562a8232c
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-05-08 02:02:40 +0900

    5.8

--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -1,13 +1,41 @@
-import sys
 from collections import deque
+import sys
 
-n = int(sys.stdin.readline())
-que = deque(range(1, n+1))
+#t = int(input())
+t = int(sys.stdin.readline())
 
-while len(que) > 1:
-    # 큐 맨 앞의 원소 건너뛰기
-    que.popleft()
-    # 그 다음 원소 삭제하기
-    que.append(que.popleft())
-
-print(que[0])
+for _ in range(0,t):
+    # p = list(input())
+    p = sys.stdin.readline().strip()
+    # n = int(input())
+    n = int(sys.stdin.readline())
+    n_list = sys.stdin.readline().strip()[1:-1].split(",")
+    # n_list = list(input())
+    # # 문자 지우기
+    # for i in n_list:
+    #     if i == "[" or i == "]" or i == ",":
+    #         n_list.remove(i)
+    
+    que = deque([])
+    # 큐에 리스트값 넣기
+    for i in n_list:
+        que.append(i)
+    
+    for i in p:
+        
+        if i == "R":
+            #뒤집기
+            que.reverse()
+        elif i == "D":
+            #버리기
+            que.popleft()
+    
+    if len(que) < 1:
+        print("error")
+    else:
+        print("[", end="")
+        for i in range(len(que)):
+            print(que[i], end="")
+            if i != len(que)-1:
+                print(",", end="")
+        print("]")
