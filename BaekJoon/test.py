162f160b72f0279393bb58b450f8d2c20d70322f
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-05-09 14:00:41 +0900

    5.9

--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -1,41 +1,11 @@
-from collections import deque
-import sys
-
-#t = int(input())
-t = int(sys.stdin.readline())
-
-for _ in range(0,t):
-    # p = list(input())
-    p = sys.stdin.readline().strip()
-    # n = int(input())
-    n = int(sys.stdin.readline())
-    n_list = sys.stdin.readline().strip()[1:-1].split(",")
-    # n_list = list(input())
-    # # 문자 지우기
-    # for i in n_list:
-    #     if i == "[" or i == "]" or i == ",":
-    #         n_list.remove(i)
+n =5 
+s = []
+for i in range(0,n):
     
-    que = deque([])
-    # 큐에 리스트값 넣기
-    for i in n_list:
-        que.append(i)
-    
-    for i in p:
-        
-        if i == "R":
-            #뒤집기
-            que.reverse()
-        elif i == "D":
-            #버리기
-            que.popleft()
-    
-    if len(que) < 1:
-        print("error")
+    if len(s) == n :
+        print(s)
     else:
-        print("[", end="")
-        for i in range(len(que)):
-            print(que[i], end="")
-            if i != len(que)-1:
-                print(",", end="")
-        print("]")
+        if n-len(s) %2 == 0 :
+            s.append(00)
+        else:
+            s.append(1)
